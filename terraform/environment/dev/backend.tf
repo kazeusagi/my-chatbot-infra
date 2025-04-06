ecb5bb04e9d9c1ece805e04962ee85aef024a5ec
kazeusagi <toshiki1098@gmail.com>
2025-04-06 11:18:08 +0000

    fix: 参照プロファイルの修正

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    region  = "ap-northeast-1"
-    bucket  = "kazeusagi-tfstate"
-    key     = "dev/my-chatbot/terraform.tfstate"
-    profile = "shared"
+    region = "ap-northeast-1"
+    bucket = "kazeusagi-tfstate"
+    key    = "dev/my-chatbot/terraform.tfstate"
   }
 }
