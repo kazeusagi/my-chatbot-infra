b0bc8d81b257c04bea6d112df04107f288506a20
kazeusagi <toshiki1098@gmail.com>
2025-04-12 08:51:36 +0000

    fix: 命名修正

--- terraform/environment/dev/data.tf
+++ terraform/environment/dev/data.tf
@@ -1,9 +1,9 @@
-data "terraform_remote_state" "test" {
+data "terraform_remote_state" "common_state" {
   backend = "s3"
   config = {
-    bucket  = "kazeusagi-tfstate"
-    key     = "my-chatbot/common/terraform.tfstate"
-    region  = "ap-northeast-1"
-    profile = "shared"
+    bucket = "kazeusagi-tfstate"
+    key    = "my-chatbot/common/terraform.tfstate"
+    region = "ap-northeast-1"
+    # profile = "shared"
   }
 }
--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -1,3 +1,3 @@
-output "test" {
-  value = data.terraform_remote_state.test.outputs.github_actions_oidc_role_arn
+output "github_actions_oidc_role_arn" {
+  value = data.terraform_remote_state.common_state.outputs.github_actions_oidc_role_arn
 }
