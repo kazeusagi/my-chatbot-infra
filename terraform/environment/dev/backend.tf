0d8923d2f2bd146d6fea194ca558837985fb0373
kazeusagi <toshiki1098@gmail.com>
2025-04-12 08:47:18 +0000

    fix: Dockerfileをnodeからdebianに変更

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    region = "ap-northeast-1"
-    bucket = "kazeusagi-tfstate"
-    key    = "my-chatbot/dev/terraform.tfstate"
-    # profile = "shared"
+    region  = "ap-northeast-1"
+    bucket  = "kazeusagi-tfstate"
+    key     = "my-chatbot/dev/terraform.tfstate"
+    profile = "shared"
   }
 }
--- terraform/environment/dev/data.tf
+++ terraform/environment/dev/data.tf
@@ -1,8 +1,9 @@
 data "terraform_remote_state" "test" {
   backend = "s3"
   config = {
-    bucket = "kazeusagi-tfstate"
-    key    = "my-chatbot/common/terraform.tfstate"
-    region = "ap-northeast-1"
+    bucket  = "kazeusagi-tfstate"
+    key     = "my-chatbot/common/terraform.tfstate"
+    region  = "ap-northeast-1"
+    profile = "shared"
   }
 }
