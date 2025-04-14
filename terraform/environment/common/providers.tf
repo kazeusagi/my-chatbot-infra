adaf41f155ca31b1879c7beef25b9856580d978b
kazeusagi <toshiki1098@gmail.com>
2025-04-14 03:36:04 +0000

    chore: test

--- terraform/environment/common/providers.tf
+++ terraform/environment/common/providers.tf
@@ -1,17 +1,16 @@
 # シンボリックリンクで共通化しているプロバイダ設定です。
 # MEMO: OIDCではprofileではなく、assume_roleを用いてアクセスを管理します。
-#       またbackend.tfではproviderを指定できないないので、デフォルトプロバイダがsharedに疎通できるように設定します。
 
-# Default: Sharedアカウントにアクセスする際のプロバイダ設定
-provider "aws" {
-  alias  = "shared"
-  region = "ap-northeast-1"
-}
-
-# TerraformAwsTemplate-Devにアクセスする際のプロバイダ設定
+# Default: TerraformAwsTemplate-Devにアクセスする際のプロバイダ設定
 provider "aws" {
   region = "ap-northeast-1"
   assume_role {
     role_arn = data.terraform_remote_state.bootstrap.outputs.github_actions_oidc_role_arn
   }
+}
+
+# Sharedアカウントにアクセスする際のプロバイダ設定
+provider "aws" {
+  alias  = "shared"
+  region = "ap-northeast-1"
 }
--- terraform/environment/dev/main.tf
+++ terraform/environment/dev/main.tf
@@ -1,7 +1,4 @@
 module "sqs" {
   source = "../../modules/sqs"
   name   = "dev-queue"
-  providers = {
-    aws = aws.terraform_aws_template_dev
-  }
 }
